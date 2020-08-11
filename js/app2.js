62bd0b7ec816eebf53e60c9a59ca20326a908562
Andrew Reynolds <andrew_reynolds@mac.com>
2020-08-11 10:53:02 -0700

    refactored validator

---
+++ js/app2.js
@@ -0,0 +1,54 @@
+// Elements on page
+const form = document.getElementById("form");
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const password2 = document.getElementById("password2");
+
+// Functions //
+
+// Check if email is valid
+function isValidEmail(email) {
+  const reg = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return reg.test(String(email).toLowerCase());
+}
+
+// Show input error message
+function showError(input, message) {
+  const formControl = input.parentElement;
+  formControl.className = "form-control error";
+
+  const small = formControl.querySelector("small");
+  small.innerText = message;
+}
+
+// Show success outline
+function showSuccess(input) {
+  const formControl = input.parentElement;
+  formControl.className = "form-control success";
+}
+
+// Get Field Name
+function getFieldName(input) {
+  let name = input.id;
+  let firstLetter = name.charAt(0).toUpperCase();
+  let restOfInput = name.slice(1);
+  name = `${firstLetter}${restOfInput}`;
+  return name;
+}
+// checkRequred Fields
+function checkRequired(inputArr) {
+  inputArr.forEach((input) => {
+    if (input.value.trim() === "") {
+      showError(input, `${getFieldName(input)} is required`);
+    } else {
+      showSuccess(input);
+    }
+  });
+}
+
+// Event Listeners //
+form.addEventListener("submit", function (e) {
+  checkRequired([username, email, password, password2]);
+  e.preventDefault();
+});
